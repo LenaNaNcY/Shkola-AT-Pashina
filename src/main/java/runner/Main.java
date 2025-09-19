ef4945735ed46fe348cb4492b7281599a735ca79
ElenaPashina <LenaPashi@gmail.com>
2025-09-19 16:06:53 +0300

    Commit 1

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
 
 	public static void main(String[] args) {
+//
+//		Triangle tr = new Triangle(1.0, 2.0, 2.0);
+//		System.out.println(tr.checkTriangle());
+//		System.out.println(tr.getMessage());
+//		System.out.println(tr.detectTriangle());
+//	}
 
-		Triangle tr = new Triangle(1.0, 2.0, 2.0);
-		System.out.println(tr.checkTriangle());
-		System.out.println(tr.getMessage());
-		System.out.println(tr.detectTriangle());
+		System.out.println("My new change");
 	}
-
 }
